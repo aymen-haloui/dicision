fe81ded8c47d5c1000b7c5db5ed1798049f1bdf7
aymen-haloui <halouiaymenabdelmouhymen@gmail.com>
2026-05-16 13:17:28 +0100

    bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,6 @@
-// Force Prisma to use the binary engine in server builds.
-// This prevents PrismaClientConstructorValidationError in build hosts where the client engine is not configured.
-process.env.PRISMA_CLIENT_ENGINE_TYPE = 'binary'
-
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { PrismaClient } = require('@prisma/client')
+const { PrismaNeonHttp } = require('@prisma/adapter-neon')
 
 declare global {
   // eslint-disable-next-line no-var
@@ -19,7 +16,10 @@ if (!process.env.DATABASE_URL) {
   process.env.DATABASE_URL = connectionString
 }
 
+const adapter = new PrismaNeonHttp(connectionString, {})
+
 export const prisma = global.prisma || new PrismaClient({
+  adapter,
   log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error'],
 })
 
